47416cdcba23b8e2649de72aa9d5c048d83e52bf
Edward Park <parkes.edward@gmail.com>
2015-10-01 15:04:05 -0400

    Delete config test files configure TwitStream filter & codes
    Deleted any testing files I had because no long needed them
    after learning how to configure both socket and twitstream
    and added bunch of server response feedbacks just in case.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var Twit     = require('twit'),              // Twitter API Client
 
 var topics = [];
 
-
 // Create a new instance of the Socket Server
 var port = 8000,
     io = new IOServer(port);
@@ -25,11 +24,49 @@ io.on('connect', function(socket) {
 
         console.log('Adding the topic "' + topic.word + '"');
     });
-    
+
     socket.on('disconnect', function() {
       console.log('Client ' + socket.id + ' disconnected.');
       topics = topics.filter(function(topic) {
           return topic.socket.id !== socket.id;
       });
   });
+});
+
+////Twit Stream
+
+var Twit   = require('twit');
+var  config = require('./authentication.js');
+var T = new Twit(config);
+var filters= {locations: '-180,-90,180,90', language: 'en'};
+var twitStream = T.stream('statuses/filter', filters);
+
+// configurating connection
+stream.on('connect', function(request) {
+    console.log('Connection attempt.');
+});
+
+stream.on('connected', function(response) {
+    console.log('Connection successful');
+});
+
+stream.on('reconnect', function(req, res, interval) {
+    console.log('Reconnecting in ' + (interval / 1e3) + ' seconds.');
+});
+
+
+//warning limits
+stream.on('warning', function(message) {
+    console.warning('Twitter warning message:');
+    console.warning(message);
+});
+
+stream.on('limit', function(message) {
+    console.log('Twitter limit message:');
+    console.log(message)
+})
+
+stream.on('disconnect', function(message) {
+    console.log('Twitter disconnection message');
+    console.log(message);
 });
