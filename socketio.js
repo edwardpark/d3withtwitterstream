7e873455991f6c7f866149b7c37a59b54f79d8f6
Edward Park <parkes.edward@gmail.com>
2015-10-01 11:21:20 -0400

    delete additional comments

--- socketio.js
+++ socketio.js
@@ -5,16 +5,13 @@ var io = new IOServer(7000);
 // Listen for new connections
 io.on('connection', function (socket) {
 
-    // Print the socket ID on connection
     console.log('Client ID ' + socket.id + ' connected.');
 
-    // The server will emit a message when receiving the `client-message` event.
     socket.on('client-message', function (data) {
         console.log(data);
         socket.emit('server-message', {msg: 'Message "' + data.msg + '" received.'});
     });
 
-    // Execute the callback if the client disconnects.
     socket.on('disconnect', function() {
         console.log('client disconnected.');
     });
