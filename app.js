5d2143ded04d8449fb9ec65e8c901bc806bc930d
Edward Park <parkes.edward@gmail.com>
2015-10-01 14:49:58 -0400

    Add 'add' event when io.on and add feedback to confirm
    added the feedback piece to check and make sure that
    the data would be actually coming through correctly
    because earlier was having issue with 'undefined' returning

--- app.js
+++ app.js
@@ -15,4 +15,15 @@ console.log('Listening for incoming connections in port ' + port);
 io.on('connect', function(socket) {
 
     console.log('Client', socket.id, 'connected.');
+
+    socket.on('add', function(topic) {
+        // Adds the new topic to the topic list
+        topics.push({
+          word: topic.word,
+          socket: socket
+        });
+
+        console.log('Adding the topic "' + topic.word + '"');
+    });
+
 });
