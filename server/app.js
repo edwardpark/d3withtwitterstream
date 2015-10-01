ea82d61def4d3e9be56cc4a3c8f2b01c5ff4c3e1
Edward Park <parkes.edward@gmail.com>
2015-10-01 10:11:52 -0400

    Add event return listeners for Twitter warning,limit and discnct
    considering the amount of information you will be processing it
    is more imperative for you to take into account and work on being
    able to keep track of what twitters feedback is going to be. Thus
    created a series of event listeners for message feedback from
    twitter in the case.

--- server/app.js
+++ server/app.js
@@ -20,3 +20,20 @@ stream.on('disconnect', function(msg) {
     console.log('Twitter disconnection message');
     console.log(msg);
 });
+
+
+// connection returns if twitter cuts off connection and why
+stream.on('warning', function(message) {
+    console.warning('Yikes! a Warning message:');
+    console.warning(message);
+});
+
+stream.on('limit', function(message) {
+    console.log('WOAH! issued a limit message:');
+    console.log(message)
+})
+
+stream.on('disconnect', function(message) {
+    console.log('NOPE! disconnection message');
+    console.log(message);
+});
