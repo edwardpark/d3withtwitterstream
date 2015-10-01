b2aa7dfd12749ee67288d950a13f6ab4d89cdffa
Edward Park <parkes.edward@gmail.com>
2015-10-01 14:41:45 -0400

    Update socket.emit for listener and connect socket server
    arbitrary move for the server to port 8000 and
    resetting connections on server to test first.

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import the Node modules
+var Twit     = require('twit'),              // Twitter API Client
+    IOServer = require('socket.io'),         // Client-side communication
+    config   = require('./authentication.js');  // Twitter Credentials
+
+var topics = [];
+
+
+// Create a new instance of the Socket Server
+var port = 8000,
+    io = new IOServer(port);
+
+console.log('Listening for incoming connections in port ' + port);
+
+io.on('connect', function(socket) {
+
+    console.log('Client', socket.id, 'connected.');
+});
